ee91e3dd4d405958a8467868f4cf1becb426f793
Nathan Beck <njosefbeck@gmail.com>
2019-04-06 18:26:11 -0500

    Add several CSS strategies

--- App/index.js
+++ App/index.js
@@ -1,8 +1,30 @@
 import html from '../html/index.js'
 import Hello from '../Hello/index.js'
+
+// This method allows us to use media queries!
+const blockStyles = () => html`
+  <style>
+    main {
+      background-color: yellow;
+    }
+
+    @media (min-width: 800px) {
+      main {
+        background-color: blue;
+      }
+    }
+  </style>
+`
+
+const styles = {
+  color: "red"
+}
+
 const App = () => html`
-  <main>
+  <main style=${styles}>
+    ${blockStyles()}
     <${Hello} name="you" />
+    <p>I'm an app!</p>
   </main>
 `
 
--- Hello/index.js
+++ Hello/index.js
@@ -1,4 +1,17 @@
 import html from '../html/index.js'
-const Hello = ({ name }) => html`<p>Hello ${name}!</p>`
+
+const logClick = (e) => {
+  e.preventDefault();
+  console.log('I have been clicked!')
+}
+
+// This works, but doesn't allow us
+// to use media queries
+const styles = {
+  backgroundColor: "red",
+  color: "black"
+}
+
+const Hello = ({ name }) => html`<p style=${styles} onClick={${logClick}}>Hello ${name}!</p>`
 
 export default Hello
