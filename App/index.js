3363fc42de1e9988298e7670c4ad897f47085456
Nathan Beck <njosefbeck@gmail.com>
2019-04-06 16:40:04 -0500

    Move <App/> to own file

---
+++ App/index.js
@@ -0,0 +1,4 @@
+import html from '../html/index.js'
+const App = ({ name }) => html`<div>Hello ${name}!</div>`
+
+export default App
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Note that this version of React is not the latest patch version
 // Would be nice to have something like that
-import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.30'
+import { ReactDOM } from 'https://unpkg.com/es-react@16.8.30'
 import html from './html/index.js'
-
-const App = ({ name }) => html`<div>Hello ${name}!</div>`
+import App from './App/index.js'
 
 ReactDOM.render(html`<${App} name="you" />`, document.getElementById('root'))
