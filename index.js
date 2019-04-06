047b2ff993a3745751a3085e6fed70a9a299de24
Nathan Beck <njosefbeck@gmail.com>
2019-04-06 16:27:22 -0500

    Add boilerplate React app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('We have liftoff!')
+// Note that this version of React is not the latest patch version
+// Would be nice to have something like that
+import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.30'
+import htm from 'https://unpkg.com/htm?module'
+
+const html = htm.bind(React.createElement)
+
+const App = ({ name }) => html`<div>Hello ${name}!</div>`
+
+ReactDOM.render(html`<${App} name="you" />`, document.getElementById('root'))
