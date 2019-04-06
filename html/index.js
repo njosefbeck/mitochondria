bb5822dac7c87ed2c3a5e8768f2efdd148380b59
Nathan Beck <njosefbeck@gmail.com>
2019-04-06 16:37:16 -0500

    Pull HTML binding into its own function

---
+++ html/index.js
@@ -0,0 +1,3 @@
+import { React } from 'https://unpkg.com/es-react@16.8.30'
+import htm from 'https://unpkg.com/htm?module'
+export default htm.bind(React.createElement)
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Note that this version of React is not the latest patch version
 // Would be nice to have something like that
 import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.30'
-import htm from 'https://unpkg.com/htm?module'
-
-const html = htm.bind(React.createElement)
+import html from './html/index.js'
 
 const App = ({ name }) => html`<div>Hello ${name}!</div>`
 
